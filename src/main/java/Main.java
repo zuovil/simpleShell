3c5197161a9da1979d2e5778caa9bf1e4b03f329
zuovil <zuohuajian@126.com>
2025-11-17 05:27:44 +0800

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        // System.out.print("$ ");
+         System.out.print("$ ");
     }
 }
