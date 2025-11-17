aa6200adf44f3d8b81c6ca9e50276a66a9c7ae12
zuovil <zuohuajian@126.com>
2025-11-17 08:16:00 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        while(true){
+        while(true) {
+            Map<String, String> pathMap = getEnv();
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
@@ -26,10 +32,51 @@ public class Main {
                     System.out.println(newStr + " is a shell builtin");
                     continue;
                 }
+
+                if(pathMap.containsKey(newStr)){
+                    System.out.println(newStr + " is " + pathMap.get(newStr));
+                    continue;
+                }
+
                 System.out.println(newStr + ": not found");
                 continue;
             }
             System.out.println(input + ": command not found");
         }
+    }
+
+    public static Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+        String osName = System.getProperty("os.name").toLowerCase();
+        String PATH = System.getenv("PATH");
+        String[] paths = PATH.split(";");
+        if(osName.contains("win")){
+            paths = PATH.split(";");
+        } else {
+            paths = PATH.split(":");
+        }
+
+        for(String path : paths){
+            File parent = new File(path);
+            if(parent.exists()) {
+                if(parent.isDirectory()) {
+                    Map<String, String> fileNameMap = Arrays.stream(Objects.requireNonNull(parent.listFiles((dir, name) -> new File(dir, name).isFile())))
+                                                        .filter(File::canExecute)
+                                                        .collect(Collectors.toMap(file -> file.getName().lastIndexOf(".") != -1 ?
+                                                                                          file.getName().substring(0, file.getName().lastIndexOf(".")) : file.getName(),
+                                                                                  File::getAbsolutePath,
+                                                                                  (v1, v2) -> v2));
+                    if(!fileNameMap.isEmpty()){
+                        env.putAll(fileNameMap);
+                    }
+                } else {
+                    if(parent.canExecute()) {
+                        String fileName = parent.getName().lastIndexOf(".") != -1 ? parent.getName().substring(0, parent.getName().lastIndexOf(".")) : parent.getName();
+                        env.put(fileName, parent.getAbsolutePath());
+                    }
+                }
+            }
+        }
+        return env;
     }
 }
