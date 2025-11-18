63d4f9abf9787016c6a7275a7a6b4790eacd57e5
zuovil <zuohuajian@126.com>
2025-11-19 05:35:30 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,65 @@ public class Main {
             String command = input.split(" ")[0];
             if("echo".equals(command)) {
                 String newStr= input.substring(input.indexOf(input.split(" ")[1]));
-                System.out.println(newStr);
+                long count = newStr.chars().filter(c -> c == '\'').count();
+                if(count == 0 || count == 1) {
+                    System.out.println(newStr.replaceAll("\\s+", " "));
+                    continue;
+                }
+                if(count % 2 == 0) {
+                    if(newStr.endsWith("'")) {
+                        System.out.println(String.join("", newStr.split("'")));
+                    } else {
+                        String after = newStr.substring(newStr.lastIndexOf("'") + 1).replaceAll("\\s+", " ");
+                        String before = newStr.substring(0, newStr.lastIndexOf("'"));
+                        String newStr1 = before + after;
+                        System.out.println(String.join("", newStr1.split("'")));
+                    }
+                    continue;
+                } else {
+                    String after = newStr.substring(newStr.lastIndexOf("'")).replaceAll("\\s+", " ");
+                    String before = newStr.substring(0, newStr.lastIndexOf("'"));
+                    System.out.println(String.join("", before.split("'")) + after);
+                    continue;
+                }
+            }
+
+            if("cat".equals(command)) {
+                String paramString = input.substring(input.indexOf(input.split(" ")[1]));
+                char[] chars = paramString.toCharArray();
+                List<Integer> singleQuoteList = new ArrayList<>();
+                List<String> params = new ArrayList<>();
+                for(int i = 0; i < chars.length; i++) {
+                    if(chars[i] == '\'') {
+                        singleQuoteList.add(i);
+                    }
+                }
+                int count = singleQuoteList.size();
+                if(!paramString.startsWith("'") || count < 2) {
+                    System.out.println();
+                    continue;
+                }
+
+                if(count % 2 != 0) {
+                    singleQuoteList.remove(singleQuoteList.size() - 1);
+                }
+
+                for (int i = 0; i < count - 1; i++) {
+                    String param = paramString.substring(singleQuoteList.get(i) + 1, singleQuoteList.get(i + 1));
+                    if(i % 2 == 0) {
+                        params.add(param);
+                    }
+                }
+                params.add(0, "cat");
+                Process process = new ProcessBuilder(params).start();
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    while (br.ready()) {
+                        System.out.println(br.readLine());
+                    }
+                }
+                process.destroy();
                 continue;
+
             }
             if("type".equals(command)) {
                 String newStr= input.substring(input.indexOf(input.split(" ")[1]));
@@ -26,7 +83,7 @@ public class Main {
                     System.out.println("type is a shell builtin");
                     continue;
                 }
-                if("echo".equals(newStr) || "exit".equals(newStr)){
+                if("echo".equals(newStr) || "exit".equals(newStr)) {
                     System.out.println(newStr + " is a shell builtin");
                     continue;
                 }
@@ -46,6 +103,7 @@ public class Main {
                         System.out.println(br.readLine());
                     }
                 }
+                process.destroy();
                 continue;
             }
             System.out.println(input + ": command not found");
@@ -56,7 +114,7 @@ public class Main {
         Map<String, String> env = new HashMap<>();
         String osName = System.getProperty("os.name").toLowerCase();
         String PATH = System.getenv("PATH");
-        String[] paths = PATH.split(";");
+        String[] paths;
         if(osName.contains("win")){
             paths = PATH.split(";");
         } else {
